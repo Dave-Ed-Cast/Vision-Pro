fdd11711847be9d5313ad3c365fc27fdd4c5913c
Dave-Ed-Cast <dcastaldi23@fed.idserve.net>
2024-05-07 12:15:02 +0200

    Initial commit. Bring it on

---
+++ XCAFootballStats/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  XCAFootballStats
+//
+//  Created by Davide Castaldi on 07/05/24.
+//
+
+import SwiftUI
+import RealityKit
+import RealityKitContent
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Model3D(named: "Scene", bundle: realityKitContentBundle)
+                .padding(.bottom, 50)
+
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview(windowStyle: .automatic) {
+    ContentView()
+}
---
+++ XCAFootballStats/XCAFootballStatsApp.swift
@@ -0,0 +1,17 @@
+//
+//  XCAFootballStatsApp.swift
+//  XCAFootballStats
+//
+//  Created by Davide Castaldi on 07/05/24.
+//
+
+import SwiftUI
+
+@main
+struct XCAFootballStatsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
