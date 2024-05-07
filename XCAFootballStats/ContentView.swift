93bbcb1d3c721434be8a48c5d3e5baa6e3c7d9d6
Dave-Ed-Cast <dcastaldi23@fed.idserve.net>
2024-05-07 12:42:12 +0200

    Stable scene

--- XCAFootballStats/ContentView.swift
+++ XCAFootballStats/ContentView.swift
@@ -11,13 +11,21 @@ import RealityKitContent
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Model3D(named: "Scene", bundle: realityKitContentBundle)
-                .padding(.bottom, 50)
-
-            Text("Hello, world!")
+        NavigationSplitView {
+            List {
+                Text ("Item")
+            }
+            .navigationTitle ("Sidebar")
+        } detail: {
+            VStack {
+                Model3D(named: "Scene", bundle: realityKitContentBundle)
+                    .padding (.bottom, 50)
+                
+                Text ("Hello, world!")
+            }
+            .navigationTitle ("Content")
+            .padding()
         }
-        .padding()
     }
 }
 
