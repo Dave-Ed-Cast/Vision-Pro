4941acddb992828a0d6f4a38862f5e2cc3dd63e7
Dave-Ed-Cast <dcastaldi23@fed.idserve.net>
2024-05-07 23:31:26 +0200

    Quick save of code

--- XCAFootballStats/Views/StandingsTableView.swift
+++ XCAFootballStats/Views/StandingsTableView.swift
@@ -15,7 +15,70 @@ struct StandingsTableView: View {
     var body: some View {
         //teamStandingtable already conforms to Identifiable
         Table(of: TeamStandingTable.self) {
-            TableColumn("W") { Text($0.wonText) }
+            
+            //TODO: Optimize this ugly code
+            
+            TableColumn ("Club") { team in
+                HStack {
+                    Text (team.positionText).fontWeight(.bold)
+                        .frame (minWidth: 20)
+                    
+                    AsyncImage(url: URL(string: team.team.crest ?? "")) { phase in
+                        switch phase {
+                        case .success(let image):
+                            image.resizable()
+                        default:
+                            Circle()
+                                .foregroundStyle(.gray.opacity (0.5))
+                        }
+                    }
+                    .frame(width: 40, height: 40)
+                }
+            }
+            .width(min: 264)
+            
+            TableColumn("W") {
+                Text($0.wonText)
+                    .frame(minWidth: 40)
+            }
+            .width(40)
+            
+            TableColumn("D") {
+                Text($0.drawText)
+                    .frame(minWidth: 40)
+            }
+            .width(40)
+            
+            TableColumn("L") {
+                Text($0.lostText)
+                    .frame(minWidth: 40)
+            }
+            .width(40)
+            
+            TableColumn("GF") {
+                Text($0.goalsForText)
+                    .frame(minWidth: 40)
+            }
+            .width(40)
+            
+            TableColumn("GA") {
+                Text($0.goalsAgainstText)
+                    .frame(minWidth: 40)
+            }
+            .width(40)
+            
+            TableColumn("GD") {
+                Text($0.goalDifferenceText)
+                    .frame(minWidth: 40)
+            }
+            .width(40)
+            
+            TableColumn("Pts") {
+                Text($0.pointsText)
+                    .frame(minWidth: 40)
+            }
+            .width(40)
+            
         } rows: {
             ForEach(vm.standings ?? []) {
                 TableRow($0)
